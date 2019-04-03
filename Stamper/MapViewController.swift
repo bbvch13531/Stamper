d815ce836b59ff2bb61d4cd59b4d2014f8f77c8f
bbvch13531 <bbvch13531@gmail.com>
2019-04-04 03:44:43 +0900

    FEAT: Successed in gc api request
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- Stamper/MapViewController.swift
+++ Stamper/MapViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import NMapsMap
+import Alamofire
+
 class MapViewController: UIViewController {
 	lazy var mapView: NMFMapView = {
 		return NMFMapView(frame: self.view.frame)
@@ -40,18 +42,22 @@ class MapViewController: UIViewController {
 		var res = requestAddress(strlat, strlng)
 	}
 	
-//	func requestAddress(_ lat: String, _ lng: String ) -> String {
-//		let headers: HTTPHeaders = [
-//		"X-NCP-APIGW-API-KEY-ID": "soxouwjilk",
-//		"X-NCP-APIGW-API-KEY": "Q9oV7NJp1RZD7negfm7A3xwmlkDN3KWvMwYlnpoO"
-//		]
-//		var result:String
-//		Alamofire.request("https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?coords=127.106338,37.362092&orders=roadaddr").responseJSON {
-//			response in
-//			print(
-//		}
-//		
-//		return
-//	}
+	func requestAddress(_ lat: String, _ lng: String ) -> String {
+		let headers: HTTPHeaders = [
+		"X-NCP-APIGW-API-KEY-ID": "soxouwjilk",
+		"X-NCP-APIGW-API-KEY": "Q9oV7NJp1RZD7negfm7A3xwmlkDN3KWvMwYlnpoO"
+		]
+		var result:String
+		var url = "https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?coords=127.106338,37.362092&orders=roadaddr&output=json"
+		Alamofire.request(url, headers: headers).responseJSON {
+			response in
+//			if let json = response.result as? [Any] {
+//					print(json)
+//			}
+				print(response)
+		}
+		
+		return ""
+	}
 }
 
